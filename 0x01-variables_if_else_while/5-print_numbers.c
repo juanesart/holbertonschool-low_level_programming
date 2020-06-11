a785c61ef7bfd3bbc81b0da1a4be51de670c1cd4
juanesart <juanesartq@gmail.com>
2020-06-11 20:36:03 +0000

    print number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - entry block
+* @void: no argument
+* Return: 0 (success)
+**/
+int main(void)
+{
+	printf("0123456789\n");
+	return (0);
+}

b2df796a9a16458bb44063a349cc87b594e5aa2d
juanesart <juanesartq@gmail.com>
2020-06-11 21:02:10 +0000

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - entry block
+* @void: no argument
+* Return: 0 (success)
+**/
+int main(void)
+{
+	int n;
+	char c;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n % 10 + '0');
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}

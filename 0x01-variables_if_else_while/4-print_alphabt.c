16f78bed41863f15b39d247fb8822fa92b6987fd
juanesart <juanesartq@gmail.com>
2020-06-11 20:26:10 +0000

    alphabet without 'eq'

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - entry block
+* @void: no argument
+* Return: 0 (success)
+**/
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c == 'e' || c == 'q')
+		{
+			c++;
+		}
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}

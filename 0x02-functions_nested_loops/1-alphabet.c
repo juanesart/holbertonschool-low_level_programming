b80d08e1c97831be826c4e51991554400ba8b12d
juanesart <juanesartq@gmail.com>
2020-06-16 12:43:04 +0000

    update

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 /**
-* main - entry block
-* @void: no argument
-* Return: 0 (success)
-**/
+ * print_alphabet - print alphabet
+ *
+ * Return: Always 0 (Success)
+ */
 void print_alphabet(void)
 {
 	char c;

e024b98ce714beab604edc59ef92cd01e4a10d09
juanesart <juanesartq@gmail.com>
2020-06-16 17:15:54 +0000

    sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * add - print the addition of two intergers
+ * @n: input parameter
+ * @m: input parameter
+ * Return: the sum
+ */
+int add(int n, int m)
+{
+	int r;
+
+	r = n + m;
+	return (r);
+}

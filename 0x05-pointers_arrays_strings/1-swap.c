ff13e0c926ffdc6257fc743b5dfa8483e817db47
juanesart <juanesartq@gmail.com>
2020-06-23 18:53:03 +0000

    swaps the values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * swap_int - swap values 
+ * @a: takes in an integer
+ * @b: takes in an integer
+ */
+void swap_int(int *a, int *b)
+{
+	int aux;
+
+	aux = *a;
+	*a = *b;
+	*b = aux;
+}

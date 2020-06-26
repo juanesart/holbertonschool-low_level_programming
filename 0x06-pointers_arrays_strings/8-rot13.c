8bb5cd94bfa5bb5e73442830f4903b83c789cee8
juanesart <juanesartq@gmail.com>
2020-06-26 04:11:25 +0000

    fixed

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -11,16 +11,16 @@ char *rot13(char *s)
 	i = 0;
 	while (s[i] != '\0')
 	{
-		while ((s[i] >= 'a' && s[i] <= 'z') ||
-		       (s[i] >= 'A' && s[i] <= 'Z'))
+		if ((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <= 'M'))
 		{
-			if ((s[i] >= 'N' && s[i] <= 'Z') ||
-			    (s[i] >= 'n' && s[i] <= 'z'))
-				s[i] = s[i] - 13;
-			else
-				s[i] = s[i] + 13;
-			i++;
+			s[i] = (s[i] + 13);
 		}
+		else
+			while ((s[i] >= 'n' && s[i] <= 'z') ||
+			      (s[i] >= 'N' && s[i] <= 'Z'))
+			{
+				s[i] = (s[i] - 13);
+			}
 		i++;
 	}
 	return (s);

19cd28b6165cd1bcb92a4c3455866915bb5cfff5
juanesart <juanesartq@gmail.com>
2020-07-04 03:16:10 +0000

    validation

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -12,7 +12,7 @@ int wildcmp(char *s1, char *s2)
 {
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
-	else if (*s1 == *s2)
+	else if (*s1 == *s2 && *s1 != '\0' && *s2 != '\0')
 		return (wildcmp(s1 + 1, s2 + 1));
 	else if (*s2 == '*')
 	{

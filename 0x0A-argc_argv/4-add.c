fffe99f3d1d87951c41cc62d4451716b49262f33
juanesart <juanesartq@gmail.com>
2020-07-06 04:41:46 +0000

    adds positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * isnumber - determines if input is a number or not
+ *
+ * @number: number to check
+ * Return: 1 if number, 0 if not
+ */
+int isnumber(char *number)
+{
+	int i;
+
+	i = 0;
+	while (number[i] != '\0')
+	{
+		if (isdigit(number[i]))
+			i++;
+		else
+			return (0);
+	}
+	return (1);
+}
+
+/**
+ * main - prints number of arguments given to program
+ * @argv: arguments
+ * @argc: number of arguments
+ * Return: always zero
+ */
+int main(int argc, char *argv[])
+{
+	int r, i;
+
+	r = 0;
+	if (argc < 3)
+	{
+		printf("0\n");
+		return (1);
+	}
+	i = 1;
+	while (i < argc)
+	{
+		if (isnumber(argv[i]))
+		{
+			r += atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+		i++;
+	}
+	printf("%d\n", r);
+	return (0);
+}

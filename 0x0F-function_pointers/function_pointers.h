9a1417617fda804cd8ef1ab32320c8573af01e2c
juanesart <juanesartq@gmail.com>
2020-07-16 02:41:04 +0000

    given as a parameter on each element of an array

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,9 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
+#include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 int _putchar(char c);
 
 #endif /* FUNCTIONS_H */

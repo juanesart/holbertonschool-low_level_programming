d31b623524a8c4d2f950ccb2910e04a10e3cc2cb
juanesart <juanesartq@gmail.com>
2020-07-26 00:11:47 +0000

    returns the sum of all its parameters

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,21 @@
+#ifndef VARIADIC_H
+#define VARIADIC_H
+#include <stdarg.h>
+#include <stdio.h>
+/**
+  * struct op - op of type struct
+  * @c: char type c
+  * @f: function associated
+  */
+typedef struct op
+{
+	char *c;
+	void (*f)();
+
+} p_t;
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+#endif /* VARIADIC_H */

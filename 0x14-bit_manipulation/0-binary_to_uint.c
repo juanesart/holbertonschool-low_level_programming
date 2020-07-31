9b16ef8fe3700cafba9cd70fc7b5ccd11abda320
juanesart <juanesartq@gmail.com>
2020-07-31 04:04:09 +0000

    converts a binary number to an unsigned int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+  * binary_to_uint - function that converts binary to unsigned int
+  * @b: takes in a conts char string
+  * Return: the converted unsigned int
+  */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int value = 0;
+
+	if (b == NULL)
+		return (0);
+	while (*b != '\0')
+	{
+		value = value << 1;
+		if (*b != '1' && *b != '0')
+			return (0);
+		else if (*b == '1')
+			value = value | 1;
+		b++;
+	}
+	return (value);
+}
